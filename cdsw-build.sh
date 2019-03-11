699e10e2d4ed7fc9df9b59374ee1450e220ae632
Timothy Spann <tspann@hortonworks.com>
2019-03-11 12:37:12 -0400

    Create cdsw-build.sh

---
+++ cdsw-build.sh
@@ -0,0 +1,31 @@
+pip3 install --upgrade pip
+pip2 install --upgrade pip
+pip2 install mxnet
+pip3 install mxnet
+pip2 install psutil
+pip3 install psutil
+pip3 install sklearn
+pip2 install sklearn
+pip2 install textblob
+pip3 install textblob
+pip2 install gluoncv
+pip3 install gluoncv
+pip2 install thrift_sasl
+pip3 install thrift_sasl
+pip3 install pyhive[hive]
+pip2 install pyhive[hive]
+pip3 install tensorflow
+pip3 install textgenrnn
+# pip2 install impyla
+# pip3 install impyla
+# pip2 install nipyapi
+# pip3 install nipyapi
+
+# hdfs dfs -mkdir -p /tmp/minifitensorflow2
+# hdfs dfs -mkdir -p /tmp/movidiussense
+
+# hdfs dfs -chmod -R 777 /tmp/
+
+# !hdfs dfs -mkdir -p /tmp/minifitensorflowp
+
+# !hdfs dfs -mkdir -p /tmp/coral
